3da02e9d80f3ea895aaf71702db79cde84d26530
bandgarvarsha <bandgarvarsha123@gmail.com>
2023-09-08 15:19:24 -0700

    commied

--- userController.js
+++ userController.js
@@ -4,7 +4,8 @@ const { secretKey } = require("./Auth/config");
 const cors = require("cors");
 const auth = require("./Auth/auth");
 const doctersList = require("./doctorsList.json");
-const { MongoClient } = require("mongodb");
+const timeSlots = require("./Auth/timeSlots.json");
+const { MongoClient, ObjectId } = require("mongodb");
 
 const app = express();
 app.use(express.json());
@@ -35,53 +36,40 @@ const corsOptions = {
 app.use(cors(corsOptions));
 const PORT = 5000;
 
-// const users = [
-//   {
-//     emailId: "bandgarvarsha@gmail.com",
-//     password: "abc123",
-//     role: "user",
-//   },
-//   {
-//     emailId: "vijay@gmail.com",
-//     password: "abc123",
-//     role: "admin",
-//   },
-//   {
-//     emailId: "bandgaronkar16@gmail.com",
-//     password: "abc123",
-//     role: "user",
-//   },
-// ];
-
 app.post("/login", async (req, res) => {
   const { emailId, password } = req.body;
 
   const db = client.db("docter");
   const collection = db.collection("registration");
-  // console.log("connected", collection);
+
   await collection
-    .findOne({ emailId: emailId })
+    .findOne({ emailId: emailId, password: password })
     .then((response) => {
       if (response) {
         const authToken = jwt.sign(
           {
             emailId: response.emailId,
             role: response.role,
-            exp: Math.floor(Date.now() / 1000) + 10 * 60,
+            // exp: Math.floor(Date.now() / 1000) + 10 * 60,
           },
           secretKey
         );
         console.log(response);
         let user = {
-          jwt: authToken,
-          fullname: response.fullName,
+          Token: authToken,
+          fullName: response.fullName,
           id: response._id,
         };
         res.send(user);
+      } else {
+        res.status(401).send({ message: "Invalid credientials" });
+
+        //throw new Error("Invalid credientials", 401);
       }
     })
-    .catch((error) => {
-      res.send(error);
+    .catch((err) => {
+      console.log("Error", err);
+      res.send(err.message);
     });
 });
 
@@ -102,25 +90,27 @@ const isValidEmail = (emailId) => {
 };
 
 app.post("/signup", async (req, res) => {
-  const { fullName, address, mobileNo, emailId, password } = req.body;
+  const { id, fullName, address, mobileNo, emailId, password } = req.body;
   console.log(await isExists(emailId));
   if (!fullName || !address || !mobileNo || !emailId || !password) {
     res.status(400).send({ message: "All fields are required" });
   } else if (!isValidEmail(emailId)) {
     res.status(409).send({ message: "Email Not Valid" });
-  } else if (await isExists(emailId)) {
+  } else if (await (!id && isExists(emailId))) {
     res.status(409).send({ message: "Email already exists" });
   } else if (!mobileNo) {
     res.status(409).send({ message: "Mobile number is not valid" });
-  } else {
-    // registeredUsers.push({
-    //   fullName,
-    //   address,
-    //   mobileNo,
-    //   emailId,
-    //   password,
-    // });
+  }
+  // } else {
+  //   if (id) {
+  //     /* author : varsha
+  //       task:Update user by Id
+  //     */
 
+  //     return res.send({ status: 200, message: "User updated.." });
+  //   }
+  else {
+    //logic for create new user
     async function createDocument(databaseName, collectionName, document) {
       const db = client.db(databaseName);
       const collection = db.collection(collectionName);
@@ -147,25 +137,88 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-app.get("/doctorList", (req, res) => {
-  // console.log(doctersList);
-  res.send(doctersList);
+app.get("/doctorList", async (req, res) => {
+  const db = client.db("docter");
+  const collection = db.collection("docterRegistration");
+
+  const col = await collection.find().toArray();
+  console.log("col", col);
+  res.send(col);
 });
 
 app.get("/searchDocter", (req, res) => {
   res.send(doctersList);
 });
 
-app.get("/docterProfile/:id", (req, res) => {
-  // console.log(req.params.id);
-  // res.send(req);
-  // console.log(req);
-  // console.log(req.query.name);
-  const id = req.params.id;
+app.get("/docterProfile/:id", async (req, res) => {
+  const db = client.db("docter");
+  const collection = db.collection("docterRegistration");
 
-  const filteredDocter = doctersList.find((docter) => docter.id == id);
-  console.log("FD:", filteredDocter);
-  res.send(filteredDocter);
+  const id = req.params.id.trim();
+  console.log("id", id);
+  await collection
+    .findOne({ _id: new ObjectId(id) })
+    .then((response) => {
+      console.log(response);
+      res.send(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//fetchUserById
+app.get("/updateUserProfile/:id", async (req, res) => {
+  const db = client.db("docter");
+  const collection = db.collection("registration");
+
+  const id = req.params.id.trim();
+
+  console.log("id", id);
+  await collection
+    .findOne({ _id: new ObjectId(id) })
+    .then((response) => {
+      console.log(response);
+      res.send(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/docterRegistration", (req, res) => {
+  res.send(req.body);
+  const {
+    id,
+    fullName,
+    education,
+    speciality,
+    address,
+    phoneNumber,
+    experience,
+    information,
+  } = req.body;
+  async function createDocument(databaseName, collectionName, document) {
+    const db = client.db(databaseName);
+    const collection = db.collection(collectionName);
+
+    try {
+      const result = await collection.insertOne(document);
+      console.log("Document inserted", result.insertedId);
+      console.log(result);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+  createDocument("docter", "docterRegistration", {
+    fullName,
+    education,
+    speciality,
+    address,
+    phoneNumber,
+    experience,
+    information,
+  });
 });
 
 app.get("/", (req, res) => {
@@ -193,6 +246,11 @@ app.get("/mongo", async (req, res) => {
   const col = await collectionName.find().toArray();
   console.log("col", col);
   res.send(col);
+});
+
+app.get("/timeSlots", (req, res) => {
+  console.log("TimeSlots : ", timeSlots);
+  res.send(timeSlots);
 });
 
 app.listen(PORT, () => {
